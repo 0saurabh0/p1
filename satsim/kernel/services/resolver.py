c7a177f1d0271eab73cd3ffa2646e42f75193025
Francesca Covella <francesca.covella@esa.int>
2021-04-29 11:26:01 +0200

    add: first resolver commit

--- satsim/kernel/services/resolver.py
+++ satsim/kernel/services/resolver.py
@@ -1,12 +1,29 @@
+from satsim import Service
 
-
-class Resolver:
+class Resolver(Service):
 
     def __init__(self):
         pass
 
     def resolve_absolute(self, absolute_path):
-        pass
+        """returns a reference to a component, field, failure container, reference,
+        event sink, event source or entry point Object in the simulation.
+        argument:
+        absolute_path: giving the absolute path string of the Object
+        behaviour:
+        If the absolute_path does not give the path to an object, it returns nullptr;
+        If no object with the given path can be found, it returns nullptr;
+        If absolute_path resolves to an object, it returns the Object reference to the object."""
+
+        if ... :
+            return None
+        elif ... :
+            return None
+        elif ... :
+            # if self._events is a dictionnary of events
+            return self._events[]
+
+
 
     def resolve_relative(self, relative_path):
         pass
