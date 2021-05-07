5d3bcef2808ecedee0d083f4a30a2d38e6c303bf
Francesca Covella <francesca.covella@esa.int>
2021-05-07 09:54:28 +0200

    work in progress: exception and model

--- satsim/kernel/model.py
+++ satsim/kernel/model.py
@@ -1,5 +1,10 @@
 from satsim import Component
 
+# models can be fallible by implementing the fallible model interface
+# models are added to the sim via the simulator add_model
+# models can be dynamically added after first startup of the simulator in the building
+# phase, also in stand-by-started
+
 
 class Model(Component):
     pass
